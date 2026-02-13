a6242f7d4ab2c0b8c19e974fe73968d2aed68f3f
MyWebGlory <166224861+mywebglory@users.noreply.github.com>
2026-02-13 18:37:23 +0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { generateSitemap } from "./scripts/generate-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/mywebglory/",
   
   server: {
     host: "::",
